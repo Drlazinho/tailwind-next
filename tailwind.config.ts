30106cc1ff3dfd50a334ec5ecb6bc1fb6e5871f6
Lázaro Pimentel <118478127+LazaroAmvox@users.noreply.github.com>
2024-02-12 01:47:12 -0300

    Upload do Avatar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,16 @@ const config: Config = {
         profile: 'max-content 1fr max-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr), minmax(0, 15rem)',
       },
-      maxWidth: {
-        app: '700px',
+      borderWidth: {
+        6: '6px',
       },
       colors: {
-        lazinho: '#00d3c3',
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+      maxWidth: {
+        app: '700px',
       },
     },
   },
